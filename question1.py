ba83c3a0e032b2a40a63f18f343e29daa37bfca7
applejuice231984 <asd9218@islomfox.ru>
2026-09-14 14:45:15 +0530

    Implement string reversal and vowel counting
    Fix string manipulation and vowel counting logic.

---
+++ question1.py
@@ -0,0 +1,39 @@
+x = str(input("enter a string in python"))
+
+#convert the string to lowercase
+
+
+#to reverse the string 
+
+
+
+
+print(x[::-1])
+
+#to uppercase conversion of the string
+
+
+x.upper()
+
+#to lowercase conversion of the string
+x.lower()
+
+###to count the number of vowels 
+
+z = "aeiouAIEOU"
+
+count = 0 
+
+for i in x :
+    if z in x : 
+        count= count+1
+
+
+    print("there are " , count , " number of variables in the string")
+
+
+###
+
+
+
+
