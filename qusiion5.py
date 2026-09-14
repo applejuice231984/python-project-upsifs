3ae3026352b43eb613bc0559b28d0445caa1f1e6
applejuice231984 <asd9218@islomfox.ru>
2026-09-14 14:48:33 +0530

    Add fruit list and update last element to Mango

---
+++ qusiion5.py
@@ -0,0 +1,11 @@
+fruits = ["Apple", "Banana", "Cherry", "Date", "Elderberry"]
+print("Original list:", fruits)
+
+print("2nd element:", fruits[1])
+print("4th element:", fruits[3])
+
+# 3. Replace the last element with "Mango"
+
+fruits[-1] = "Mango"
+
+print("Updated list:", fruits)
